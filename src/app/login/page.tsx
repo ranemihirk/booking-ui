05bc2a8119b60b32f21b0515f9d0312b0cd3e533
Mihir Rane <ranemihirk@gmail.com>
2024-02-29 23:43:30 +0530

    Added Login and register basic pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+// pages/login.tsx
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Add login form here */}
+    </div>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+// pages/register.tsx
+import React from "react";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      {/* Add registration form here */}
+    </div>
+  );
+}
